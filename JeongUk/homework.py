e9199b35a68f6f753894278ac55a106db8db1736
JungUk <osy_fe@naver.com>
2021-04-01 21:09:52 +0900

    동생과제 대리해줌^^7

---
+++ JeongUk/homework.py
@@ -0,0 +1,21 @@
+var1 = input()
+var2 = input()
+
+a = float(var1)
+b = float(var2)
+
+print("** 덧셈 연산 **\n")
+result1 = a + b
+print(a, " + ", b, " = ", result1)
+
+print("** 뺄셈 연산 **\n")
+result1 = a - b
+print(a, " - ", b, " = ", result1)
+
+print("** 곱셈 연산 **\n")
+result1 = a * b
+print(a, " * ", b, " = ", result1)
+
+print("** 나눗셈 연산 **\n")
+result1 = a / b
+print(a, " / ", b, " = ", result1)
